cb4aa26377d307ad9d32487894dc47ae3d95e7ac
Danila Rogozhnikov <94745416+m0libdEn@users.noreply.github.com>
2022-10-05 02:26:04 -0400

    Changes for already done things
    Now if you click on our list it will be green color, that shown by the green card (will be icons soon)

--- js/script.js
+++ js/script.js
@@ -2,14 +2,23 @@ const button = document.getElementById('btn')
 const list = document.getElementById('list')
 const info = document.getElementById('input')
 
-let todo = function(){
+let createElement = function(){
     let element = document.createElement("div")
     element.className = "list-element"
     element.innerHTML = info.value
+    let done = document.createElement("i")
+    done.className="fas fa-check-square"
+    done.style.display = "none"
     list.appendChild(element)
+    element.addEventListener('click', ()=>{
+        element.classList.toggle('active')
+    })
+    element.before(done)
 }
 
-button.addEventListener('click', todo)
+button.addEventListener('click', function() {
+    createElement()
+})
 info.addEventListener('keyup', function(el) {
 if (el.code === 'Enter') { button.click()}
 })
